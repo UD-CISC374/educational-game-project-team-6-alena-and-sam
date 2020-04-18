804ffe8af7fbcecc53274508516979fc76166931
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-04-17 17:46:44 -0700

    Added buttons to move funds

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,20 +2,17 @@ import ExampleObject from '../objects/exampleObject';
 import Dragon from '../objects/dragon';
 
 export default class MainScene extends Phaser.Scene {
-  private exampleObject: ExampleObject;
-  private funds401k;
-  private fundsStockA;
-  private fundsStockB;
-  private fundsStockC;
-  private Savings;
-  private Checking;
+
+  public funds401k: number;
+  private fundsStock;
+  public Checking: number;
   private Bar401;
   private BarA;
-  private BarB;
-  private BarC;
-  private BarSavings;
   private BarChecking;
-  private idleArrow;
+  private InvestArrowUp;
+  private InvestArrowDown;
+  private StockArrowUp;
+  private StockArrowDown;
 
 
   dragon: Phaser.GameObjects.Sprite;
@@ -28,31 +25,60 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.funds401k = 0;
-    this.fundsStockA = 0;
-    this.fundsStockB = 0;
-    this.fundsStockC = 0;
-    this.Savings = 0;
-    this.Checking = 0;
+    this.fundsStock = 0;
+    this.Checking = 100;
 
 
     this.Bar401 = this.add.bitmapText(0, 0, "pixelFont", "   401K: $"+ this.funds401k, 16);
-    this.BarA = this.add.bitmapText(75, 0, "pixelFont", "Stock A: $"+ this.fundsStockA, 16);
-    this.BarB = this.add.bitmapText(150, 0, "pixelFont", "Stock B: $"+ this.fundsStockB, 16);
-    this.BarC = this.add.bitmapText(225, 0, "pixelFont", "Stock C: $"+ this.fundsStockC, 16);
-    this.BarSavings = this.add.bitmapText(300, 0, "pixelFont", "Savings: $"+ this.Savings, 16);
+    this.BarA = this.add.bitmapText(75, 0, "pixelFont", "Stock : $"+ this.fundsStock, 16);
     this.BarChecking = this.add.bitmapText(375, 0, "pixelFont", "Checking: $"+ this.Checking, 16);
 
-    this.idleArrow = this.add.image(100, 40, "arrow");
-    this.idleArrow.scale = 0.05;
+    this.InvestArrowUp = this.add.sprite(100, 40, "arrow");
+    this.InvestArrowUp.scale = 0.05;
+    this.InvestArrowUp.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.buttonMoveAdd401K(this, this.InvestArrowUp));
+
+    this.InvestArrowDown = this.add.sprite(200, 40, "arrow");
+    this.InvestArrowDown.scale = 0.05;
+    this.InvestArrowDown.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.buttonMoveMinus401K(this, this.InvestArrowDown) );
+
+    this.StockArrowDown = this.add.sprite(200, 80, "arrow");
+    this.StockArrowDown.scale = 0.05;
+    this.StockArrowDown.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.buttonMoveMinusStock(this, this.StockArrowDown) );
+
+    this.StockArrowUp = this.add.sprite(100, 80, "arrow");
+    this.StockArrowUp.scale = 0.05;
+    this.StockArrowUp.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.buttonMoveAddStock(this, this.StockArrowDown) );
+
 
     this.dragon = new Dragon(this, "dragon", this.scale.width/8, this.scale.height/1.3);
     this.dragon.scale = 0.5;
   }
 
-  moveFunds(accountA, accountB, amount){
-    if ((amount <= accountA) && (amount>0)){
-      accountA -= amount;
-      accountB += amount;
+  buttonMoveAdd401K(pointer, gameObject){
+    this.moveFundsto401K(10);
+  }
+  buttonMoveMinus401K(pointer, gameObject){
+    this.moveFundsto401K(-10);
+  }
+  moveFundsto401K(amount){
+    if (((amount <= this.Checking) && (amount>0))||(((-1)*amount <= this.funds401k) && (amount<0))){
+      this.Checking -= amount;
+      this.funds401k += amount;
+      this.updateAccounts();
+    }
+  }
+
+  buttonMoveAddStock(pointer, gameObject){
+    this.moveFundstoStock(10);
+  }
+  buttonMoveMinusStock(pointer, gameObject){
+    this.moveFundstoStock(-10);
+  }
+  moveFundstoStock(amount){
+    if (((amount <= this.Checking) && (amount>0))||(((-1)*amount <= this.fundsStock) && (amount<0))){
+      this.Checking -= amount;
+      this.fundsStock += amount;
+      this.updateAccounts();
     }
   }
 
@@ -64,6 +90,15 @@ export default class MainScene extends Phaser.Scene {
     this.Checking -= amount;
   }
 
+  updateAccounts(){
+    this.BarChecking.text = "Checking: $"+ this.Checking;
+    this.Bar401.text = "401K: $"+ this.funds401k;
+    this.BarA.text = "Stock: $" + this.fundsStock;
+
+  }
+
+
   update() {
+    
   }
 }
