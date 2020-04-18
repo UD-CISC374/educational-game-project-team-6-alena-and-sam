d67034484d857206ab379cbae2d1a3e037fa0f61
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-04-17 18:25:31 -0700

    Updates

---
+++ precache-manifest.e844ffe2a8621dabb2bb8b4c032d1a67.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e46c202f525e64de76d681945af5349",
+    "url": "assets/arrow_button_active.png"
+  },
+  {
+    "revision": "dadb4a6578080791a5a3ecb15cd2ff1d",
+    "url": "assets/arrow_button_idle.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "assets/font.xml"
+  },
+  {
+    "revision": "03311ef719c17bd35912e8d4b916f175",
+    "url": "assets/fossil_cave.jpg"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "assets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "assets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "1557ec678fe51b5ab510dc591279efbc",
+    "url": "assets/images/dragon_boi.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "3172addb6984821ea6b1975e96c4ed4f",
+    "url": "index.html"
+  },
+  {
+    "revision": "89618c32566084975f25",
+    "url": "main.f1a886479c7e8a16ca4a.bundle.js"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "vendors.0805bb80903235d40eaf.bundle.js"
+  }
+]);
