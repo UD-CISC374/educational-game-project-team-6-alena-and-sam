82a1727e594fe76d2784fe153b53595c5c880e20
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-04-23 09:50:05 -0700

    Updates

--- alpha/precache-manifest.7f46a56a1809e202647f323907e11397.js
+++ alpha/precache-manifest.7f46a56a1809e202647f323907e11397.js
@@ -1,5 +1,9 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
+    "revision": "98e79ecca8d2fb7a6ce62bea5d98a785",
+    "url": "/educational-game-project-team-6-alena-and-sam/alphaassets/01coin.png"
+  },
+  {
     "revision": "9e46c202f525e64de76d681945af5349",
     "url": "/educational-game-project-team-6-alena-and-sam/alphaassets/arrow_button_active.png"
   },
@@ -48,6 +52,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/educational-game-project-team-6-alena-and-sam/alphaassets/images/dragon_boi.png"
   },
   {
+    "revision": "f1a73a912b6d6e1d8c036df03e5e7719",
+    "url": "/educational-game-project-team-6-alena-and-sam/alphaassets/jester frog.PNG"
+  },
+  {
     "revision": "4fcf9ce53de642d871dca40e4365dec7",
     "url": "/educational-game-project-team-6-alena-and-sam/alphaassets/moon_sun_star.png"
   },
@@ -56,15 +64,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/educational-game-project-team-6-alena-and-sam/alphafavicon.ico"
   },
   {
-    "revision": "f14b4bfec982462389b7",
-    "url": "/educational-game-project-team-6-alena-and-sam/alphagen.main.545f27ffe6fc2c7e298b.bundle.js"
+    "revision": "f9d68fe91f219e049bb3",
+    "url": "/educational-game-project-team-6-alena-and-sam/alphagen.main.c4d3ff3f6c880b3cdf4b.bundle.js"
   },
   {
     "revision": "5e69403aa42e950fedff",
     "url": "/educational-game-project-team-6-alena-and-sam/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
   },
   {
-    "revision": "05183c6233f6b5ef6cb1c79c435a64dd",
+    "revision": "86fa22201bc531e45eed9918e2f155c8",
     "url": "/educational-game-project-team-6-alena-and-sam/alphaindex.html"
   },
   {
--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-team-6-alena-and-sam/alphaprecache-manifest.d41f4710fea0b90e246695ecfadf800e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-6-alena-and-sam/alphaprecache-manifest.7f46a56a1809e202647f323907e11397.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
