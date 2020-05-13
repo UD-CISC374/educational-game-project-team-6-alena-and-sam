f641e93e4712d596176c4b130d70cb2f5a1c520b
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-05-13 14:20:53 -0700

    Improve Frog Store Text

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -11,7 +11,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image("day", "assets/moon_sun_star.png");
     this.load.image("jesterFrog", "assets/jester frog.PNG");
     this.load.spritesheet("coin", "assets/01coin.png", {frameWidth: 120, frameHeight: 120});
-    this.load.image("frogStore", "assets/FrogStore.png");
+    this.load.image("frogStore", "assets/FrogStore2.png");
     this.load.image("news", "assets/NewsIcon.jpg");
     this.load.image("stockCrashNews", "assets/StockCrash.jpg");
     this.load.image("buyButton", "assets/BuyButton.png");
